19e86a6af12e94acaca9cf8f844707c40e34e63f
Jelle ten Brinke <jmtenbrinke@gmail.com>
2017-04-29 11:45:18 +0200

    Implement DatabaseAdapter and remove the foreign key tables (unnecesary)

--- business/src/main/java/com/roundsquare/wally/business/database/Database.java
+++ business/src/main/java/com/roundsquare/wally/business/database/Database.java
@@ -20,6 +20,8 @@ public class Database {
     public static final String TABLE_TRANSACTIONS = "transactions";
     public static final String DB_CREATE_TRANSACTIONS = "create table " + TABLE_TRANSACTIONS
             + " (" + S.TRANSACTIONID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + S.ACCOUNTID + " INTEGER, "
+            + S.CATEGORYID + " INTEGER, "
             + S.DESCRIPTION + " TEXT NOT NULL, "
             + S.NOTE + " TEXT, "
             + S.VALUE + " TEXT NOT NULL);";
@@ -28,22 +30,6 @@ public class Database {
     public static final String DB_CREATE_CATEGORIES = "create table " + TABLE_CATEGORIES
             + " (" + S.CATEGORYID + " INTEGER PRIMARY KEY AUTOINCREMENT"
             + S.NAME + " TEXT NOT NULL);";
-
-    public static final String TABLE_ACCOUNT_TRANSACTIONS = "account_transactions";
-    public static final String DB_CREATE_ACCOUNT_TRANSACTIONS = "create table " + TABLE_ACCOUNT_TRANSACTIONS
-            + " (" + S.ACCOUNT_TRANSACTION_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + S.ACCOUNTID + " INTEGER, "
-            + S.TRANSACTIONID + " INTEGER, "
-            + "foreign key(" + S.ACCOUNTID + ") references " + TABLE_ACCOUNTS + "(" + S.ACCOUNTID + "),"
-            + "foreign key(" + S.TRANSACTIONID + ") references " + TABLE_TRANSACTIONS + "(" + S.TRANSACTIONID + "));";
-
-    public static final String TABLE_CATEGORY_TRANSACTIONS = "account_transactions";
-    public static final String DB_CREATE_CATEGORY_TRANSACTIONS = "create table " + TABLE_CATEGORY_TRANSACTIONS
-            + " (" + S.CATEGORY_TRANSACTION_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + S.CATEGORYID + " INTEGER, "
-            + S.TRANSACTIONID + " INTEGER, "
-            + "foreign key(" + S.CATEGORYID + ") references " + TABLE_CATEGORIES + "(" + S.CATEGORYID + "),"
-            + "foreign key(" + S.TRANSACTIONID + ") references " + TABLE_TRANSACTIONS + "(" + S.TRANSACTIONID + "));";
 
     public static final String DB_NAME = "wally_db";
     public static final int DB_VERSION = 1;
@@ -68,8 +54,6 @@ public class Database {
             db.execSQL(DB_CREATE_ACCOUNTS);
             db.execSQL(DB_CREATE_TRANSACTIONS);
             db.execSQL(DB_CREATE_CATEGORIES);
-            db.execSQL(DB_CREATE_ACCOUNT_TRANSACTIONS);
-            db.execSQL(DB_CREATE_CATEGORY_TRANSACTIONS);
         }
 
         @Override
--- business/src/main/java/com/roundsquare/wally/business/database/DatabaseAdapter.java
+++ business/src/main/java/com/roundsquare/wally/business/database/DatabaseAdapter.java
@@ -1,9 +1,14 @@
 package com.roundsquare.wally.business.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
 import com.roundsquare.wally.business.model.Account;
 import com.roundsquare.wally.business.model.Category;
 import com.roundsquare.wally.business.model.Transaction;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 
 
@@ -11,65 +16,142 @@ import java.util.HashMap;
  * Created by Jelle on 28/04/2017.
  */
 
-public class DatabaseAdapter implements IDatabaseAdapter {
+public class DatabaseAdapter extends Database implements IDatabaseAdapter {
+
+    public DatabaseAdapter(Context context) {
+        super(context);
+    }
 
     @Override
     public HashMap<Long, Account> getAccounts() {
-        return null;
+        HashMap<Long, Account> accounts = new HashMap<>();
+
+        Cursor c = db.rawQuery("SELECT * FROM " + TABLE_ACCOUNTS, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            long id = c.getLong(c.getColumnIndex(S.ACCOUNTID));
+            String name = c.getString(c.getColumnIndex(S.NAME));
+            BigDecimal startingValue = new BigDecimal(c.getString(c.getColumnIndex(S.STARTING_VALUE)));
+
+            accounts.put(id, new Account(id, name, startingValue));
+
+            c.moveToNext();
+        }
+        c.close();
+        return accounts;
+
     }
 
     @Override
     public long addAccount(Account account) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.NAME, account.getName());
+        values.put(S.STARTING_VALUE, account.getStartingValue().toString());
+
+        return db.insert(TABLE_ACCOUNTS, null, values);
     }
 
     @Override
     public long removeAccount(Account account) {
-        return 0;
+        return db.delete(TABLE_ACCOUNTS, S.ACCOUNTID + "=" + account.getId(), null);
     }
 
     @Override
     public long updateAccount(Account account) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.NAME, account.getName());
+        values.put(S.STARTING_VALUE, account.getStartingValue().toString());
+
+        return db.update(TABLE_ACCOUNTS, values, S.ACCOUNTID + "=" + account.getId(), null);
     }
 
     @Override
     public HashMap<Long, Transaction> getTransactions() {
-        return null;
+        HashMap<Long, Transaction> transactions = new HashMap<>();
+
+        Cursor c = db.rawQuery("SELECT * FROM " + TABLE_TRANSACTIONS, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            long id = c.getLong(c.getColumnIndex(S.TRANSACTIONID));
+            long accountid = c.getLong(c.getColumnIndex(S.ACCOUNTID));
+            long categoryid = c.getLong(c.getColumnIndex(S.CATEGORYID));
+            String description = c.getString(c.getColumnIndex(S.DESCRIPTION));
+            String note = c.getString(c.getColumnIndex(S.NOTE));
+            BigDecimal value = new BigDecimal(c.getString(c.getColumnIndex(S.VALUE)));
+
+            transactions.put(id, new Transaction(id, accountid, categoryid, description, note, value));
+
+            c.moveToNext();
+        }
+        c.close();
+        return transactions;
     }
 
     @Override
     public long addTransaction(Transaction transaction) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.ACCOUNTID, transaction.getAccountId());
+        values.put(S.CATEGORYID, transaction.getCategoryId());
+        values.put(S.DESCRIPTION, transaction.getDescription());
+        values.put(S.NOTE, transaction.getNote());
+        values.put(S.VALUE, transaction.getValue().toString());
+
+        return db.insert(TABLE_TRANSACTIONS, null, values);
     }
 
     @Override
     public long removeTransaction(Transaction transaction) {
-        return 0;
+        return db.delete(TABLE_TRANSACTIONS, S.TRANSACTIONID + "=" + transaction.getId(), null);
     }
 
     @Override
     public long updateTransaction(Transaction transaction) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.ACCOUNTID, transaction.getAccountId());
+        values.put(S.CATEGORYID, transaction.getCategoryId());
+        values.put(S.DESCRIPTION, transaction.getDescription());
+        values.put(S.NOTE, transaction.getNote());
+        values.put(S.VALUE, transaction.getValue().toString());
+
+        return db.update(TABLE_TRANSACTIONS, values, S.TRANSACTIONID + "=" + transaction.getId(), null);
     }
 
     @Override
     public HashMap<Long, Category> getCategories() {
-        return null;
+        HashMap<Long, Category> categories = new HashMap<>();
+
+        Cursor c = db.rawQuery("SELECT * FROM " + TABLE_CATEGORIES, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            long id = c.getLong(c.getColumnIndex(S.CATEGORYID));
+            String name = c.getString(c.getColumnIndex(S.NAME));
+
+            categories.put(id, new Category(id, name));
+
+            c.moveToNext();
+        }
+        c.close();
+        return categories;
     }
 
     @Override
     public long addCategory(Category category) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.NAME, category.getName());
+
+        return db.insert(TABLE_CATEGORIES, null, values);
     }
 
     @Override
     public long removeCategory(Category category) {
-        return 0;
+        return db.delete(TABLE_CATEGORIES, S.CATEGORYID + "=" + category.getId(), null);
     }
 
     @Override
     public long updateCategory(Category category) {
-        return 0;
+        ContentValues values = new ContentValues();
+        values.put(S.NAME, category.getName());
+
+        return db.update(TABLE_CATEGORIES, values, S.CATEGORYID + "=" + category.getId(), null);
     }
 }
